393e76d71356f5634f0c40918a831e0e12c6009d
saifalihdd <saifalialhaddad2@gmail.com>
2026-07-15 22:25:24 +0700

    build: configure static export and basePath for Portfolio repository

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  
+  // Menyesuaikan dengan nama repositori persis seperti di URL (Case Sensitive)
+  basePath: '/Portfolio', 
+  
+  // Menonaktifkan optimasi gambar bawaan Next.js (Wajib untuk GitHub Pages / Static HTML)
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
